859ecfbeac351fbefde22d51b7621e8c5c273909
Francois Ehrhardt <frehrh@microsoft.com>
2020-12-16 17:41:34 +0100

    Updates windows implementation to vnext

--- windows/RNDocumentPicker/RCTDocumentPickerModule.cs
+++ windows/RNDocumentPicker/RCTDocumentPickerModule.cs
@@ -1,5 +1,5 @@
-﻿using Newtonsoft.Json.Linq;
-using ReactNative.Bridge;
+﻿using Microsoft.ReactNative.Managed;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Linq;
 using System.Collections.Generic;
@@ -10,78 +10,76 @@ using Windows.Storage.Pickers;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.IO;
+using Microsoft.ReactNative;
 
 namespace RNDocumentPicker
 {
-    class RCTDocumentPickerModule : ReactContextNativeModuleBase, ILifecycleEventListener
+    [ReactModule]
+    class RCTDocumentPickerModule
     {
         private FileOpenPicker _pendingPicker;
         private bool _isInForeground;
 
-	private static readonly String E_FAILED_TO_SHOW_PICKER = "FAILED_TO_SHOW_PICKER";
-	private static readonly String E_DOCUMENT_PICKER_CANCELED = "DOCUMENT_PICKER_CANCELED";
-	private static readonly String E_UNEXPECTED_EXCEPTION = "UNEXPECTED_EXCEPTION";
+        private static readonly String E_FAILED_TO_SHOW_PICKER = "FAILED_TO_SHOW_PICKER";
+        private static readonly String E_DOCUMENT_PICKER_CANCELED = "DOCUMENT_PICKER_CANCELED";
+        private static readonly String E_UNEXPECTED_EXCEPTION = "UNEXPECTED_EXCEPTION";
 
-	private static readonly String OPTION_TYPE = "type";
-    private static readonly String CACHE_TYPE = "cache";
-    private static readonly String OPTION_MULIPLE = "multiple";
-    private static readonly String OPTION_READ_CONTENT = "readContent";
-    private static readonly String FIELD_URI = "uri";
-    private static readonly String FIELD_FILE_COPY_URI = "fileCopyUri";
-	private static readonly String FIELD_NAME = "name";
-	private static readonly String FIELD_TYPE = "type";
-	private static readonly String FIELD_SIZE = "size";
-    private static readonly String FIELD_CONTENT = "content";
+        private static readonly String OPTION_TYPE = "type";
+        private static readonly String CACHE_TYPE = "cache";
+        private static readonly String OPTION_MULIPLE = "multiple";
+        private static readonly String OPTION_READ_CONTENT = "readContent";
+        private static readonly String FIELD_URI = "uri";
+        private static readonly String FIELD_FILE_COPY_URI = "fileCopyUri";
+        private static readonly String FIELD_NAME = "name";
+        private static readonly String FIELD_TYPE = "type";
+        private static readonly String FIELD_SIZE = "size";
+        private static readonly String FIELD_CONTENT = "content";
 
-        public RCTDocumentPickerModule(ReactContext reactContext)
-            : base(reactContext)
+        [ReactInitializer]
+        public void Initialize(ReactContext reactContext)
         {
-        }
-
-        public override string Name
-        {
-            get
-            {
-                return "RNDocumentPicker";
-            }
-        }
-
-        public override void Initialize()
-        {
-            Context.AddLifecycleEventListener(this);
-        }
-
-        public void OnSuspend()
-        {
-            _isInForeground = false;
-        }
-
-        public void OnResume()
-        {
+            // Here we should probably be listening to lifecycle events to know the
+            // state of the application and respond to OnSuspend and OnResume events.
+            // Something like: 
+            // reactContext.AddLifecycleEventListener(this);
+            // It looks like this may not be supported yet:
+            // https://microsoft.github.io/react-native-windows/docs/reactnativehost-api
             _isInForeground = true;
         }
 
-        public void OnDestroy()
-        {
-        }
+        //public void OnSuspend()
+        //{
+        //    _isInForeground = false;
+        //}
 
-        [ReactMethod]
-        public void pick(JObject options, IPromise promise)
+        //public void OnResume()
+        //{
+        //    _isInForeground = true;
+        //    // TODO/ question: shouldn't we resume _pendingPicker here?
+        //}
+
+        //public void OnDestroy()
+        //{
+        //}
+
+        [ReactMethod("pick")]
+        public void Pick(JSValue options, ReactPromise<JSValue> promise)
         {
+            var optionsObj = options.AsObject();
             try
             {
                 FileOpenPicker openPicker = new FileOpenPicker();
                 openPicker.ViewMode = PickerViewMode.Thumbnail;
                 openPicker.SuggestedStartLocation = PickerLocationId.DocumentsLibrary;
                 // Get file type array options
-                var fileTypeArray = options.Value<JArray>(OPTION_TYPE);
-                var cache = options.Value<Boolean>(CACHE_TYPE);
+                var fileTypeArray = optionsObj[OPTION_TYPE].AsArray();
+                var cache = optionsObj.ContainsKey(CACHE_TYPE) && optionsObj[CACHE_TYPE].AsBoolean();
                 // Init file type filter
                 if (fileTypeArray != null && fileTypeArray.Count > 0)
                 {
-                    foreach (String typeString in fileTypeArray)
+                    foreach (JSValue typeString in fileTypeArray)
                     {
-                        List<String> types = typeString.Split(' ').ToList();
+                        List<String> types = typeString.AsString().Split(' ').ToList();
                         foreach (String type in types)
                         {
                             if (Regex.Match(type, "(^[.]+[A-Za-z0-9]*$)|(^[*]$)").Success)
@@ -102,8 +100,8 @@ namespace RNDocumentPicker
                     {
                         if (_isInForeground)
                         {
-                            var isMultiple = options.Value<bool>(OPTION_MULIPLE);
-                            var readContent = options.Value<bool>(OPTION_READ_CONTENT);
+                            var isMultiple = optionsObj.ContainsKey(OPTION_MULIPLE) && optionsObj[OPTION_MULIPLE].AsBoolean();
+                            var readContent = optionsObj.ContainsKey(OPTION_READ_CONTENT) && optionsObj[OPTION_READ_CONTENT].AsBoolean();
                             if (isMultiple)
                             {
                                 await PickMultipleFileAsync(openPicker, cache, readContent, promise);
@@ -120,16 +118,19 @@ namespace RNDocumentPicker
                     }
                     catch (Exception ex)
                     {
-                        promise.Reject(E_FAILED_TO_SHOW_PICKER, ex.Message);
+                        var error = new ReactError() { Code = E_FAILED_TO_SHOW_PICKER, Exception = ex, Message = ex.Message };
+                        promise.Reject(error);
                     }
                 });
             }
-            catch (Exception ex) {
-                promise.Reject(E_UNEXPECTED_EXCEPTION, ex.Message);
+            catch (Exception ex)
+            {
+                var error = new ReactError() { Code = E_UNEXPECTED_EXCEPTION, Exception = ex, Message = ex.Message };
+                promise.Reject(error);
             }
         }
 
-        private async Task<JObject> PrepareFile(StorageFile file, Boolean cache, Boolean readContent)
+        private async Task<JSValueObject> PrepareFile(StorageFile file, Boolean cache, Boolean readContent)
         {
             String base64Content = null;
             if (readContent)
@@ -151,7 +152,7 @@ namespace RNDocumentPicker
                 var fileInCache = await file.CopyAsync(ApplicationData.Current.TemporaryFolder, file.Name.ToString(), NameCollisionOption.ReplaceExisting).AsTask().ConfigureAwait(false);
                 var basicProperties = await fileInCache.GetBasicPropertiesAsync();
 
-                return new JObject {
+                return new JSValueObject {
                     { FIELD_URI, fileInCache.Path },
                     { FIELD_FILE_COPY_URI, fileInCache.Path },
                     { FIELD_TYPE, fileInCache.ContentType },
@@ -160,10 +161,11 @@ namespace RNDocumentPicker
                     { FIELD_CONTENT, base64Content }
                 };
             }
-            else {
+            else
+            {
                 var basicProperties = await file.GetBasicPropertiesAsync();
 
-                return new JObject {
+                return new JSValueObject {
                     { FIELD_URI, file.Path },
                     { FIELD_FILE_COPY_URI, file.Path },
                     { FIELD_TYPE, file.ContentType },
@@ -174,44 +176,51 @@ namespace RNDocumentPicker
             }
         }
 
-        private async Task<bool> PickMultipleFileAsync(FileOpenPicker picker, Boolean cache, Boolean readContent, IPromise promise) {
+        private async Task<bool> PickMultipleFileAsync(FileOpenPicker picker, Boolean cache, Boolean readContent, ReactPromise<JSValue> promise)
+        {
             IReadOnlyList<StorageFile> files = await picker.PickMultipleFilesAsync().AsTask().ConfigureAwait(false);
             if (files.Count > 0)
             {
-                JArray jarrayObj = new JArray();
-                foreach (var file in files) {
+                JSValueArray jarrayObj = new JSValueArray();
+                foreach (var file in files)
+                {
                     jarrayObj.Add(PrepareFile(file, cache, readContent).Result);
                 }
                 promise.Resolve(jarrayObj);
             }
             else
             {
-                promise.Reject(E_DOCUMENT_PICKER_CANCELED, "User canceled document picker");
+                var error = new ReactError() { Code = E_DOCUMENT_PICKER_CANCELED, Message = "User canceled document picker" };
+                promise.Reject(error);
             }
 
             return true;
         }
 
-        private async Task<bool> PickSingleFileAsync(FileOpenPicker picker, Boolean cache, Boolean readContent, IPromise promise) {
+        private async Task<bool> PickSingleFileAsync(FileOpenPicker picker, Boolean cache, Boolean readContent, ReactPromise<JSValue> promise)
+        {
             var file = await picker.PickSingleFileAsync().AsTask().ConfigureAwait(false);
             if (file != null)
             {
-                JArray jarrayObj = new JArray();
-                jarrayObj.Add(PrepareFile(file, cache, readContent).Result);
+                JSValueArray jarrayObj = new JSValueArray
+                {
+                    PrepareFile(file, cache, readContent).Result
+                };
                 promise.Resolve(jarrayObj);
             }
             else
             {
-                promise.Reject(E_DOCUMENT_PICKER_CANCELED, "User canceled document picker");
+                var error = new ReactError() { Code = E_DOCUMENT_PICKER_CANCELED, Message = "User canceled document picker" };
+                promise.Reject(error);
             }
 
             return true;
         }
 
-        private void OnInvoked(Object error, Object success, ICallback callback)
-        {
-            callback.Invoke(error, success);
-        }
+        //private void OnInvoked(Object error, Object success, ICallback callback)
+        //{
+        //    callback.Invoke(error, success);
+        //}
 
         private static async void RunOnDispatcher(DispatchedHandler action)
         {
---
+++ windows/RNDocumentPicker/ReactPackageProvider.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.ReactNative;
+using Microsoft.ReactNative.Managed;
+
+namespace RNDocumentPicker
+{
+    public partial class ReactPackageProvider : IReactPackageProvider
+    {
+        public void CreatePackage(IReactPackageBuilder packageBuilder)
+        {
+            CreatePackageImplementation(packageBuilder);
+        }
+
+        /// <summary>
+        /// This method is implemented by the C# code generator
+        /// </summary>
+        partial void CreatePackageImplementation(IReactPackageBuilder packageBuilder);
+    }
+}
