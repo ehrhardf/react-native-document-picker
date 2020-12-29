8eb5aca4ae80a0ce9540d800f99f64f908281dde
Francois Ehrhardt <francois.ehrhardt@gmail.com>
2020-12-29 17:11:52 +0100

    Update RCTDocumentPickerModule.cs

--- windows/RNDocumentPicker/RCTDocumentPickerModule.cs
+++ windows/RNDocumentPicker/RCTDocumentPickerModule.cs
@@ -14,7 +14,7 @@ using Microsoft.ReactNative;
 
 namespace RNDocumentPicker
 {
-    [ReactModule]
+    [ReactModule("RNDocumentPicker")]
     class RCTDocumentPickerModule
     {
         private FileOpenPicker _pendingPicker;
